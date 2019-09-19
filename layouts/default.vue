72baa8771b7ea4893f61574087876dec0abadda3
Anthony ESTEBE <anthony@mesg.com>
2019-09-19 15:20:00 +0700

    create layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <el-container style="max-width: 1024px; margin: auto;">
+    <el-header style="height: auto; text-align: center; padding: 1em;">
+      <el-image :src="require('~/assets/logo.png')" fit="contain" />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
 </template>
+
+<script>
+export default {}
+</script>
 
 <style>
 html {
@@ -22,34 +31,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>

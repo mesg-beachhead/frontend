125e747c841f314e6c8c170e6353ab897bc4383e
Anthony ESTEBE <anthony@mesg.com>
2019-09-19 15:20:25 +0700

    create index page

---
+++ components/offer/Thumb.vue
@@ -0,0 +1,39 @@
+<template>
+  <nuxt-link :to="`/offers/${offer.id}`" class="item-thumb">
+    <el-card shadow="hover" :header-style="{ padding: 0 }">
+      <template v-slot:header>
+        <el-image :src="offer.item.image" fit="fill" />
+      </template>
+      <h4>{{ offer.item.name }}</h4>
+      <p>{{ offer.item.description }}</p>
+    </el-card>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    offer: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  display: block;
+}
+
+.el-image {
+  display: block;
+}
+</style>
+
+<style>
+.item-thumb > .el-card > .el-card__header {
+  padding: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,30 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        beachhead
-      </h1>
-      <h2 class="subtitle">
-        Beachhead marketplace
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <el-container>
+    <el-row :gutter="12">
+      <el-col
+        :xs="24"
+        :sm="12"
+        :md="6"
+        v-for="offer in offers"
+        :key="offer.id"
+        style="margin-bottom: 1em;"
+      >
+        <offer-thumb :offer="offer" />
+      </el-col>
+    </el-row>
+  </el-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import { mapGetters } from 'vuex'
+import OfferThumb from '~/components/offer/Thumb'
 export default {
   components: {
-    Logo
-  }
+    OfferThumb
+  },
+  fetch: ({ store }) => store.dispatch('offer/fetch'),
+  computed: mapGetters({
+    offers: 'offer/items'
+  })
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>

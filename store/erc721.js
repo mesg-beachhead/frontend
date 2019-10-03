a938968052a389f70b677cac5280c7c3835170e1
Anthony ESTEBE <anthony@mesg.com>
2019-10-03 17:34:50 +0700

    add contract stores

--- store/erc721.js
+++ store/erc721.js
@@ -1,6 +1,6 @@
 import contractStore from '../lib/contractStore'
 
 export default contractStore(
-  '0x51c683a707D6EE96369bdF89AE96181773e40c07',
-  require('./erc20.abi.json')
+  '0xc58d3e5A609BdeCD913cEf6996CDd8355bdE933c',
+  require('./erc721.abi.json')
 )
---
+++ store/marketplace.js
@@ -0,0 +1,6 @@
+import contractStore from '../lib/contractStore'
+
+export default contractStore(
+  '0xDcbA375C9ACaa1961F2Ca19d801f749250384e1B',
+  require('./marketplace.abi.json')
+)

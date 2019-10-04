39ab4c75bb68618fa333d830a8ddf8ce0468b61e
Anthony ESTEBE <anthony@mesg.com>
2019-10-04 09:48:04 +0700

    approve and create offer for marketplace

--- store/marketplace.js
+++ store/marketplace.js
@@ -1,6 +1,28 @@
 import contractStore from '../lib/contractStore'
 
-export default contractStore(
+const contract = contractStore(
   '0xDcbA375C9ACaa1961F2Ca19d801f749250384e1B',
   require('./marketplace.abi.json')
 )
+
+export default {
+  ...contract,
+  actions: {
+    ...contract.actions,
+    create: async (
+      { dispatch, getters },
+      { store, tokenId, currency, price }
+    ) => {
+      await dispatch('erc721/approve', [getters.address, tokenId], {
+        root: true
+      })
+      const offer = await dispatch('createOffer', [
+        store,
+        tokenId,
+        currency,
+        parseInt(price, 10)
+      ])
+      return offer
+    }
+  }
+}
